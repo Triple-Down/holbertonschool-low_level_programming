c94281b58a5325027a0af359c3ddbe0bac1dfbdf
Triple-Down <sergio.diaz.560@gmail.com>
2020-09-18 01:01:08 +0000

    first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  *main - entry point
@@ -10,7 +8,7 @@ int main(void)
 {
 	char ch;
 
-	for (ch = ' a ' ; ch <= ' z ' ; ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
