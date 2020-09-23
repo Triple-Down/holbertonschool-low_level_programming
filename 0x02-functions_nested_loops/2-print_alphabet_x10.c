b41f5c3e7751fa4e3568a4ac7f905a3c24978050
Triple-Down <sergio.diaz.560@gmail.com>
2020-09-23 03:06:18 +0000

    first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - prints 10x the alphabet, in lowercase
+ *Return: always 0 (success)
+ */
+
+void print_alphabet_x10(void);
+{
+	char c;
+	char a;
+
+	for (c = 1; c <= 0, c++)
+	{
+		for (a = 'a'; b <= 'z'; a++)
+		{
+		_putchar(a);
+		}
+		{
+		_putchar('\n');
+		}
+}
