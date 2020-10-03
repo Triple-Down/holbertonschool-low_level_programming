e763cbfbdd3c805850bcb70be05ea12065983283
Triple-Down <sergio.diaz.560@gmail.com>
2020-10-03 20:53:51 +0000

    added #5

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,13 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
+int _putchar(char c); //0
+void print_alphabet(void); //1
+void print_alphabet_x10(void); //2
+int _islower(int c); //3
+int _isalpha(int c); //4
+int print_sign(int n); //5
 
-void jack_bauer(void);
+void jack_bauer(void); //8
 
 #endif
